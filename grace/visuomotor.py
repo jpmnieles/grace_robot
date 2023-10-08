efbee60992962db05b114a80626713b0aa687bb5
jpmnieles <jpmnieles@connect.ust.hk>
2023-10-09 01:37:00 +0800

    Added initial people attention module
    Signed-off-by: jpmnieles <jpmnieles@connect.ust.hk>

--- grace/visuomotor.py
+++ grace/visuomotor.py
@@ -13,6 +13,78 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Bool
 from cv_bridge import CvBridge, CvBridgeError
 
+import cv2
+import dlib
+
+
+class PeopleAttention(object):
+
+    def __init__(self) -> None:
+        self.person_detected = False
+
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(os.path.join(os.getcwd(),'pretrained','shape_predictor_68_face_landmarks.dat'))
+        dlib.cuda.set_device(0)
+
+    def register_imgs(self, left_img, right_img):
+        self.left_img = left_img
+        self.right_img  = right_img
+        self.detect_people(self.left_img, self.right_img)
+
+    def detect_people(self, left_img, right_img):
+        # Detection       
+        self.l_gray = cv2.cvtColor(left_img.copy(), cv2.COLOR_BGR2GRAY)
+        self.r_gray = cv2.cvtColor(right_img.copy(), cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the grayscale image
+        self.l_detections = self.detector(self.l_gray, 0)
+        self.r_detections = self.detector(self.r_gray, 0)
+
+        # Person detected or not
+        if len(self.l_detections) > 0 or len(self.r_detections) > 0:
+            self.person_detected = True
+        else:
+            self.person_detected = False
+
+    def process_img(self, eye):
+        if eye == 'left':
+            img = self.left_img
+        elif eye == 'right':
+            img = self.right_img
+        img = self.ctr_cross_img(img)
+        return img
+
+    def get_pixel_target(self, id, eye: str):
+        if len(self.l_detections) > 0:
+            landmarks = self.predictor(self.l_gray, self.l_detections[id])
+            x_target = landmarks.part(30).x
+            y_target = landmarks.part(30).y
+            delta_x = x_target - 317.13846547
+            delta_y =  219.22972847 - y_target
+    
+    def _px_to_deg_fx(self, x):
+        x = math.atan(x/569.4456315)
+        x = math.degrees(x)
+        return x
+
+    def _px_to_deg_fy(self, x):
+        x = math.atan(x/571.54490033)
+        x = math.degrees(x)
+        return x
+    
+    def ctr_cross_img(self, img):
+        img = cv2.line(img, (315, 0), (315, 480), (0,255,0))
+        img = cv2.line(img, (0, 202), (640, 202), (0,255,0))
+        img = cv2.drawMarker(img, (315,202), color=(0, 255, 0), markerType=cv2.MARKER_CROSS, markerSize=15, thickness=2)
+        return img
+    
+    def display_target(self, delta_x, delta_y, img):
+        abs_x = 314.69441889 + delta_x
+        abs_y = 201.68845842 - delta_y
+        disp_img = cv2.drawMarker(img, (round(abs_x),round(abs_y)), color=(255, 0, 0), markerType=cv2.MARKER_TILTED_CROSS, markerSize=13, thickness=2)
+        return disp_img
+
+
 
 class VisuoMotorNode(object):
     
@@ -29,11 +101,11 @@ class VisuoMotorNode(object):
         self.ats = message_filters.ApproximateTimeSynchronizer([self.left_eye_sub, self.right_eye_sub], queue_size=1, slop=0.015)
         self.ats.registerCallback(self._eye_imgs_callback)
         # self.motors_ready_sub = rospy.Subscriber('/motors_ready', Bool, self._motors_ready_callback)
-        # self.attention = PeopleAttention()
+        self.attention = PeopleAttention()
         # self.calibration = BaselineCalibration()
         self.display_l_img_pub = rospy.Publisher('/left_eye/image_processed', Image, queue_size=1)
         self.display_r_img_pub = rospy.Publisher('/right_eye/image_processed', Image, queue_size=1)
-        self.motors_ready = False
+        self.motors_ready = True
     
     # def _motors_ready_callback(self, msg):
     #     self.motors_ready = msg.data
@@ -43,17 +115,17 @@ class VisuoMotorNode(object):
         self.right_img = self.bridge.imgmsg_to_cv2(right_img_msg, "bgr8")
         # print(left_img_msg.header, right_img_msg.header)
         
-        # if self.motors_ready:
-        #     # self.attention.register_imgs(self.left_img, self.right_img)
-        #     # if self.attention.people_detected:
-        #     #     id = self.attention.get_target()
-        #     #     x_l, y_l = self.attention.get_pixel_coord(id, 'left')
-        #     #     x_r, y_r = self.attention.get_pixel_coord(id, 'right')
-        #     #     self.left_img = self.attention.process_img('left')
-        #     #     self.right_img = self.attention.process_img('right')
+        if self.motors_ready:
+            self.attention.register_imgs(self.left_img, self.right_img)
+            if self.attention.person_detected:
+                id = 0  # self.attention.get_target()
+        #     #     dx_l, dy_l = self.attention.get_pixel_coord(id, 'left')
+        #     #     dx_r, dy_r = self.attention.get_pixel_coord(id, 'right')
+                self.left_img = self.attention.process_img('left')
+                self.right_img = self.attention.process_img('right')
                 
-        #     #     theta_l_pan, theta_l_tilt = self.calibration.compute_left_img(x_l, x_r)
-        #     #     theta_r_pan, theta_r_tilt = self.calibration.compute_right_img(x_l, x_r)
+        #     #     theta_l_pan, theta_l_tilt = self.calibration.compute_left_img(dx_l, dy_1)
+        #     #     theta_r_pan, theta_r_tilt = self.calibration.compute_right_img(dx_l, dy_r)
         #     #     theta_tilt = self.calibration.compute_tilt(theta_l_tilt, theta_r_tilt)
         #     #     delta_theta_max = self.calibration.store_command(theta_l_pan, theta_r_pan, theta_tilt)
         #     #     self.motor_pub.publish(theta_l_pan, theta_r_pan, theta_tilt, delta_theta_max)
