a25400dc3c66143054e9dff7fa0499bbdb6b1ed6
jpmnieles <jpmnieles@connect.ust.hk>
2023-10-31 00:09:08 +0800

    Added simple gaze tool
    Signed-off-by: jpmnieles <jpmnieles@connect.ust.hk>

--- grace/gaze.py
+++ grace/gaze.py
@@ -5,8 +5,8 @@ from geometry_msgs.msg import Point, PointStamped
 from sensor_msgs.msg import Image, CameraInfo
 import tf
 
-rospy.init_node("gaze_animation")
 
+rospy.init_node("gaze_animation")
 head_focus_pub = rospy.Publisher('/hr/animation/set_face_target', Target, queue_size=1)
 gaze_focus_pub = rospy.Publisher('/hr/animation/set_gaze_target', Target, queue_size=1)
 tf_listener = tf.TransformListener(False, rospy.Duration.from_sec(1))
@@ -23,7 +23,6 @@ class PosStruct():
         self.x = x
         self.y = y
         self.z = z
-
     
 
 def get_blender_pos(pos, ts, frame_id):
@@ -53,6 +52,7 @@ def pos_to_target(pos, speed):
     msg.speed = speed
     return msg
 
+
 def SetGazeFocus(pos, speed, ts, frame_id='robot'):
     try:
         pos = get_blender_pos(pos, ts, frame_id)
@@ -73,21 +73,26 @@ def SetHeadFocus(pos, speed, ts, frame_id='robot', proportional=True, head_yaw_m
     except Exception as e:
         print("Head focus exception: {}".format(e))
 
+
 def UpdateGaze(pos, ts, frame_id="realsense"):
     SetGazeFocus(pos, 5.0, ts, frame_id)
     SetHeadFocus(pos, 1, ts, frame_id)
 
 
-def depth_image_callback(msg):
-    ts = msg.header.stamp
-    x = 1
-    y = 0.3
-    z = -0.5
+def calculate_normalized_px(u,v):
+    x = 1.0
+    y = (320-u)/320
+    z = (240-v)/480
     pos = PosStruct()
     pos.set_xyz(x,y,z)
-    UpdateGaze(pos, rospy.Time.now()-rospy.Time.from_sec(0.06),'realsense')
+    return pos
 
 
-depth_sub = rospy.Subscriber('/hr/perception/jetson/realsense/camera/aligned_depth_to_color/image_raw', Image, depth_image_callback)
+rate = rospy.Rate(30)
 while not rospy.is_shutdown():
-    rospy.spin()
+    print('-----------')
+    u = eval(input('u (px):'))
+    v = eval(input('v (px):'))
+    pos =calculate_normalized_px(u,v)
+    UpdateGaze(pos, rospy.Time.now()-rospy.Time.from_sec(0.06),'realsense')
+    rate.sleep()
