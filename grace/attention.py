4a95726439443f9fcd5ac46b0dc560a503d14041
jpmnieles <jpmnieles@connect.ust.hk>
2023-11-20 12:18:38 +0800

    Transferred attention module to another file
    Signed-off-by: jpmnieles <jpmnieles@connect.ust.hk>

---
+++ grace/attention.py
@@ -0,0 +1,68 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import cv2
+import dlib
+
+from grace.utils import *
+
+
+class PeopleAttention(object):
+
+    def __init__(self) -> None:
+        self.camera_mtx = load_camera_mtx()
+        self.person_detected = False
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(os.path.join(os.getcwd(),'pretrained','shape_predictor_68_face_landmarks.dat'))
+        dlib.cuda.set_device(0)
+
+    def register_imgs(self, left_img, right_img):
+        self.left_img = left_img
+        self.right_img  = right_img
+
+    def detect_people(self, left_img, right_img):
+        # Detection       
+        self.l_gray = cv2.cvtColor(left_img.copy(), cv2.COLOR_BGR2GRAY)
+        self.r_gray = cv2.cvtColor(right_img.copy(), cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the grayscale image
+        self.l_detections = self.detector(self.l_gray, 0)
+        self.r_detections = self.detector(self.r_gray, 0)
+
+        # Person detected or not
+        if len(self.l_detections) > 0 or len(self.r_detections) > 0:
+            self.person_detected = True
+            # print(self.camera_mtx)
+        else:
+            self.person_detected = False
+
+    def get_pixel_target(self, id:int, eye:str):
+        """id (int): select from [0, 1, ...]
+        eye (str): select from ['left_eye', 'right_eye']
+        """
+        if eye == 'left_eye':
+            detection = self.l_detections[id]
+            img = self.l_gray
+        elif eye == 'right_eye':
+            detection = self.r_detections[id]
+            img = self.r_gray
+        landmarks = self.predictor(img, detection)
+        x_target = landmarks.part(30).x
+        y_target = landmarks.part(30).y
+        delta_x = x_target - self.camera_mtx[eye]['cx']
+        delta_y =  self.camera_mtx[eye]['cy'] - y_target
+        return delta_x, delta_y
+    
+    def visualize_target(self, delta_x, delta_y, img, id:int, eye:str):
+        """eye (str): select from ['left_eye', 'right_eye']
+        """
+        if eye == 'left_eye':
+            detection = self.l_detections[id]
+        elif eye == 'right_eye':
+            detection = self.r_detections[id]
+        cv2.rectangle(img, (detection.left(), detection.top()), (detection.right(), detection.bottom()), (0, 0, 255), 2)
+        abs_x = self.camera_mtx[eye]['cx'] + delta_x
+        abs_y = self.camera_mtx[eye]['cy'] - delta_y
+        disp_img = cv2.drawMarker(img, (round(abs_x),round(abs_y)), color=(255, 0, 0), markerType=cv2.MARKER_TILTED_CROSS, markerSize=13, thickness=2)
+        return disp_img
