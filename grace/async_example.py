67f8361dc0435ee012cfc579b07026dd23db1aab
jpmnieles <jpmnieles@connect.ust.hk>
2023-11-20 10:49:06 +0800

    Updated the async example
    Signed-off-by: jpmnieles <jpmnieles@connect.ust.hk>

--- grace/async_example.py
+++ grace/async_example.py
@@ -4,27 +4,72 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 import random
 import time
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import message_filters
+
 
 class MyNode:
     def __init__(self):
         # Initialize ROS node
         rospy.init_node('my_node')
-
+        self.motors_ready = False
         # Create a publisher
-        self.pub = rospy.Publisher('left_eye/processed_example', String, queue_size=2)
+        self.pub = rospy.Publisher('left_eye/processed_example', String, queue_size=1)
+        self.pub2 = rospy.Publisher('left_eye/processed_header', String, queue_size=1)
 
         # Subscribe to the input topic and specify the callback as the message handler
-        self.left_eye_sub = rospy.Subscriber('/left_eye/image_raw', Image, self.callback, queue_size=2)
         self.old_stamp = rospy.Time.now()
+        self.bridge = CvBridge()
+        self.sub = rospy.Subscriber('/left_eye/image_raw', Image, self.pub_callback, queue_size=1)
+        self.left_eye_sub = message_filters.Subscriber('/left_eye/image_raw', Image)
+        self.sub2 = rospy.Subscriber('left_eye/processed_header', String, self.sub_callback, queue_size=1)
+        self.display_r_img_pub = rospy.Publisher('/right_eye/image_processed', Image, queue_size=1)
 
-    def callback(self, msg):
+    def pub_callback(self, msg):
         # Perform processing based on the received message
         # Publish a new message based on the processing result
-        delay = random.uniform(0.1, 1.0)
+        delay = random.uniform(0.16, 0.25)
+        if random.random() > 0.5:
+            delay = 0.0
         time.sleep(delay)
-        
-        fps = self.get_fps(msg.header.stamp)
-        self.pub.publish("FPS: %f" % fps)
+        self.pub2.publish(str(msg.header))
+
+    
+    def sub_callback(self, msg):
+        # Perform processing based on the received message
+        # Publish a new message based on the processing result
+        self.motors_ready = True
+
+    def callback(self, msg, msg2):
+        # Perform processing based on the received message
+        # Publish a new message based on the processing result
+        if self.motors_ready:
+            fps = self.get_fps(msg.header.stamp)
+            self.pub.publish("FPS: %f" % fps)
+            left_eye_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+
+            # Define the text to be added
+            text = "FPS: %d" % fps
+
+            # Choose the font type, font scale, and color
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 1.2
+            color = (255, 0, 0)  # BGR color format (here, blue)
+
+            # Determine the text size
+            text_size, _ = cv2.getTextSize(text, font, font_scale, 2)
+
+            # Calculate the position to place the text (bottom-left corner)
+            text_position = (10, left_eye_img.shape[0] - 10)
+
+            # Add the text to the image
+            cv2.putText(left_eye_img, text, text_position, font, font_scale, color, 2)
+            self.display_r_img_pub.publish(self.bridge.cv2_to_imgmsg(left_eye_img, encoding="bgr8"))
+            cv2.imshow('async', left_eye_img)
+            cv2.waitKey(1)
+            print('meron')
+        self.motors_ready = False
 
     def get_fps(self, new_stamp):
         fps = 1/(new_stamp-self.old_stamp).to_sec()
