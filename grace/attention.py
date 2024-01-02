a5f393b7b67aff3a99f4726cf8020646fad27eaf
jpmnieles <jpmnieles@connect.ust.hk>
2024-01-02 17:36:35 +0800

    Added chessboard attention
    Signed-off-by: jpmnieles <jpmnieles@connect.ust.hk>

--- grace/attention.py
+++ grace/attention.py
@@ -66,3 +66,42 @@ class PeopleAttention(object):
         abs_y = self.camera_mtx[eye]['cy'] - delta_y
         disp_img = cv2.drawMarker(img, (round(abs_x),round(abs_y)), color=(255, 0, 0), markerType=cv2.MARKER_TILTED_CROSS, markerSize=13, thickness=2)
         return disp_img
+
+
+class ChessboardAttention(object):
+
+    def __init__(self) -> None:
+        self.get_camera_mtx()
+
+    def get_camera_mtx(self, filename='config/camera/camera_mtx.json'):
+        with open(filename, 'r') as stream:
+            data = json.load(stream)
+            self.l_mtx = np.array(data['left_eye']['camera_matrix'])
+            self.l_dist = np.array(data['left_eye']['distortion_coefficients'])
+            self.r_mtx = np.array(data['right_eye']['camera_matrix'])
+            self.r_dist = np.array(data['right_eye']['distortion_coefficients'])
+            self.c_mtx = np.array(data['chest_cam']['camera_matrix'])
+            self.c_dist = np.array(data['chest_cam']['distortion_coefficients'])
+            return self.l_mtx, self.l_dist, self.r_mtx, self.r_dist, self.c_mtx, self.c_dist
+
+    def visualize_chess_idx(self, chess_idx, chess_list, img):
+        x, y = chess_list[chess_idx]
+        img = cv2.drawMarker(img, (round(x),round(y)), color=(255, 0, 0), 
+                       markerType=cv2.MARKER_TILTED_CROSS, markerSize=13, thickness=2)
+        return img
+
+    def process_img(self, chess_idx, img, camera):
+        px = (None,None)
+        chess_idx = 21
+
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+        if ret == True:
+            corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+            s_corners = corners2.squeeze()
+            px = s_corners[chess_idx]
+            img = self.visualize_chess_idx(chess_idx, s_corners, img)
+
+        return px, img
