e9147f68eed31dfe0dadfda0fdd942dbe4e6126c
jpmnieles <jpmnieles@connect.ust.hk>
2024-01-08 23:31:18 +0800

    Reworked env to include inside the visuomotor node
    Signed-off-by: jpmnieles <jpmnieles@connect.ust.hk>

--- grace/env.py
+++ grace/env.py
@@ -1,5 +1,6 @@
 import rospy
 from std_msgs.msg import String, Float64MultiArray
+from sensor_msgs.msg import JointState, Image
 
 import json
 import time
@@ -10,6 +11,8 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 
+from grace.tf_gaze import VisuoMotorNode
+
 
 class GraceEnv(gym.Env):
 
@@ -18,13 +21,8 @@ class GraceEnv(gym.Env):
     def __init__(self, max_steps=10):
         super(GraceEnv, self).__init__()
 
-        rospy.init_node('grace_state_subscriber')
-        self.state_sub = rospy.Subscriber('/grace/state', String, self._grace_state_callback, queue_size=1)
-        self.action_pub = rospy.Publisher('/grace/action', Float64MultiArray, queue_size=1)
-        
-        self.state_lock = threading.Lock()
-        self.condition = threading.Condition()
-
+        rospy.init_node('grace_env')
+        self.grace = VisuoMotorNode()
         self.seed_value = 143
         # self.reset(0, 0)
         self.max_steps = max_steps  # Maximum number of steps before termination of episode
@@ -34,13 +32,6 @@ class GraceEnv(gym.Env):
                                        high=np.array([0.30679615757712825, 0.30679615757712825, 0.38349519697141027]),
                                        shape=(3,))  # Continuous action space of size 1
         self.observation_space = spaces.Box(low=-1, high=-1, shape=(3,))
-
-    def _grace_state_callback(self, msg):
-        with self.condition:
-            dict_data = json.loads(msg.data)
-            with self.state_lock:
-                self.state = dict_data
-            self.condition.notify()
 
     def seed(self, seed_value):
         self.seed_value = seed_value
@@ -58,18 +49,19 @@ class GraceEnv(gym.Env):
         # reward = -np.abs(self.target_phi - self.current_phi)  # Negative absolute difference between the current y and the target y
         reward = 0
 
-        action = [random.randint(-8,8), random.randint(-8,8), random.randint(-8,8)]
-        msg = Float64MultiArray()
-        msg.data = action
-
         print('=============')
+        with self.grace.buffer_lock:
+            print('PREV_STATE:', self.grace.rl_state)
+        action = [random.randint(-8,8), random.randint(-8,8), random.randint(-8,8)]
         print('Action:', action)
 
-        self.action_pub.publish(msg)
-        with env.condition:
-            env.condition.wait()
-            with env.state_lock:
-                print(env.state)
+        self.grace.set_action(action)
+        print('set_action')
+        rospy.sleep(1)
+
+        with self.grace.buffer_lock:
+            print('NEXT_STATE:', self.grace.rl_state)
+        print('=============')
 
         done = False
         if self.step_count >= self.max_steps or reward > -0.2:
@@ -82,6 +74,6 @@ if __name__ == '__main__':
 
     # Test the environment
     env = GraceEnv()
-
     while not rospy.is_shutdown():
         env.step()
+        rospy.sleep(2)
